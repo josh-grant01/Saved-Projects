a04cf1501e62c33d1d60ed8ff9a4bd2c9d32af08
josh-grant01 <31286178+josh-grant01@users.noreply.github.com>
2018-10-11 14:55:06 -0600

    Used to test homework

---
+++ Test Files/src/test/files/TestFiles.java
@@ -0,0 +1,14 @@
+package test.files;
+
+/**
+ *
+ * @author jgrant
+ */
+public class TestFiles
+{
+    public static void main(String[] args)
+    {
+        //System.out.println(Math.ceil(Math.sqrt(17)));
+        System.out.println(Math.random() * 20 + 10);
+    }
+}
