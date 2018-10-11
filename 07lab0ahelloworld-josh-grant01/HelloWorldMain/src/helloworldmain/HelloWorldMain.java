646afc7f7dea8328895e47335a80c9380d98e18a
josh-grant01 <31286178+josh-grant01@users.noreply.github.com>
2018-10-11 14:58:10 -0600

    Hello World Lab
    ASCII art

---
+++ 07lab0ahelloworld-josh-grant01/HelloWorldMain/src/helloworldmain/HelloWorldMain.java
@@ -0,0 +1,55 @@
+/* edit your file according to the directions provided. As you make changes 
+to the file, comment out the previous section so I see all your work.
+*/
+package helloworldmain;
+/*
+* Name: Josh Grant
+*
+* Program Name - HelloWorld
+* Program Description - My first java program
+*/
+
+public class HelloWorldMain
+{
+        public static void main (String[] args)
+        {
+                //System.out.println("Hello World");
+                /*System.out.println("Here's another line to print.");
+                System.out.println("And another line to print.");
+                System.out.println("Here's the last line.");*/
+                //The diference between print and println is that println prints to the screen then enters onto a new line
+                System.out.println("    ________________           |*\\_/*|__________");
+                System.out.println("   |   ___________   |        | |_/-\\_|_______  |");
+                System.out.println("   |  |           |  |        |  |           |  |");
+                System.out.println("   |  |   O   O   |  |        |  |   O   O   |  |");
+                System.out.println("   |  |     -     |  |        |  |     -     |  |");
+                System.out.println("   |  |   \\___/   |  |        |  |   \\___/   |  |");
+                System.out.println("   |  |___     ___|  |        |  |___________|  |");
+                System.out.println("   |______|\\_/|______|        |_________________|");
+                System.out.println("      _|__|/ \\|_|_..............._|__________|_");
+                System.out.println("     / *********** \\            / ************ \\");
+                System.out.println("   /  *************  \\        /  **************  \\");
+                System.out.println("  ---------------------      ----------------------");
+                
+                
+                //Make your own ASCII art creation
+                System.out.println("\n\n\n\n\n\n");
+                System.out.println("             _________");
+                System.out.println("|       |        |");
+                System.out.println("|       |        |");
+                System.out.println("|-------|        |");
+                System.out.println("|       |        |");
+                System.out.println("|       |        |");
+                System.out.println("             ---------");
+                
+                /*
+             _________
+|       |        |
+|       |        |
+|-------|        |
+|       |        |
+|       |        |
+             ---------
+                */
+        }
+}
