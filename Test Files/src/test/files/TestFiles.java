ed41a001ca6a99982e9b1ab6ae9595d69511bfb9
drago <drago@GRANTFAMILY01>
2018-11-17 11:17:39 -0700

    Used to Test Homework 11-17-18 11:17 AM

--- Test Files/src/test/files/TestFiles.java
+++ Test Files/src/test/files/TestFiles.java
@@ -9,6 +9,12 @@ public class TestFiles
     public static void main(String[] args)
     {
         //System.out.println(Math.ceil(Math.sqrt(17)));
-        System.out.println(Math.random() * 20 + 10);
+        //System.out.println(Math.random() * 20 + 10);
+        int x = 17;
+        System.out.println(!(x != 17));
+        char letter = 'Q';
+        System.out.println(!(letter == 'Q'));
+        String bob = "joe";
+        System.out.println(!bob.equals("joe"));
     }
 }
